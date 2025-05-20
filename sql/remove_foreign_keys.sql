9ebe784394e5474ef6e6e22406a586b481517f95
Lucho Pérez <luchopb@hotmail.com>
2025-05-20 10:38:36 -0300

    Cambios

--- sql/remove_foreign_keys.sql
+++ sql/remove_foreign_keys.sql
@@ -8,4 +8,8 @@ ALTER TABLE pagos
 DROP FOREIGN KEY pagos_ibfk_1; -- foreign key de contrato_id
 
 -- Limpiar la tabla de propiedades
-TRUNCATE TABLE propiedades; 
+SET FOREIGN_KEY_CHECKS = 0;
+TRUNCATE TABLE propiedad_imagenes;
+TRUNCATE TABLE propiedades;
+SET FOREIGN_KEY_CHECKS = 1;
+
